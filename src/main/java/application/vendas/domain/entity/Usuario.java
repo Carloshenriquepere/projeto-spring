f875baf04c27f4498aff91cb243708fc5e7ce64e
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2023-04-20 19:08:45 -0300

    adding JWT and Security class

---
+++ src/main/java/application/vendas/domain/entity/Usuario.java
@@ -0,0 +1,36 @@
+package application.vendas.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    @NotEmpty(message = "{campo.login.obrigatorio}")
+    private String login;
+
+    @Column
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
+    private String senha;
+
+    @Column
+    private boolean admin;
+
+
+}
