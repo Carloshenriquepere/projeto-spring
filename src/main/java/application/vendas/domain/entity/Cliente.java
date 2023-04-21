737dd4e618bddce5600c3125b7d186a0bad7a236
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2023-04-21 16:02:06 -0300

    introducing MySQL

--- src/main/java/application/vendas/domain/entity/Cliente.java
+++ src/main/java/application/vendas/domain/entity/Cliente.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotEmpty( message = " {campo.nome.obrigatorio}")
